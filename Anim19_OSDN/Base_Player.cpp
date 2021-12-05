b7b265dbdec2f1a95af26fdcbc4338824c0ff95c
hgtterry <hgtterry@fa3e1370-42a8-4b12-9444-c83572d2b46e>
2021-12-05 12:28:26 +0000

    Equity15 051221 Base Player

---
+++ Anim19_OSDN/Base_Player.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "GD19_App.h"
+#include "Base_Player.h"
+
+
+Base_Player::Base_Player()
+{
+}
+
+
+Base_Player::~Base_Player()
+{
+}
---
+++ Anim19_OSDN/Base_Player.h
@@ -0,0 +1,8 @@
+#pragma once
+class Base_Player
+{
+public:
+	Base_Player();
+	~Base_Player();
+};
+
