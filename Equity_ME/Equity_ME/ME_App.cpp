c107655040b2c1ada539bff2c7ad740ff23ce419
hgtterry <hgtterry@fa3e1370-42a8-4b12-9444-c83572d2b46e>
2022-01-09 15:48:55 +0000

    Equity15 090122 Equity_ME First

---
+++ Equity_ME/Equity_ME/ME_App.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "ME_App.h"
+
+
+ME_App::ME_App()
+{
+	EquityDirecory_FullPath[0] = 0;
+}
+
+
+ME_App::~ME_App()
+{
+}
---
+++ Equity_ME/Equity_ME/ME_App.h
@@ -0,0 +1,10 @@
+#pragma once
+class ME_App
+{
+public:
+	ME_App();
+	~ME_App();
+
+	char EquityDirecory_FullPath[MAX_PATH];
+};
+
