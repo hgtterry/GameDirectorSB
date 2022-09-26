546327e1aea2a7f9026b4f959b5aca60a6c93f96
hgtterry <hgtterry@fa3e1370-42a8-4b12-9444-c83572d2b46e>
2022-09-26 09:29:56 +0000

    Equity_SB 260922 Camera Base

--- Anim19_OSDN/Base_Camera.cpp
+++ Anim19_OSDN/Base_Camera.cpp
@@ -7,4 +7,22 @@ Base_Camera::Base_Camera()
 
 Base_Camera::~Base_Camera()
 {
+
+	Init_Camera();
+
+}
+
+// *************************************************************************
+// *	  		Init_Camera:- Terry and Hazel Flanigan 2022				   *
+// *************************************************************************
+void Base_Camera::Init_Camera(void)
+{
+	CamPos.x = 0;
+	CamPos.y = 0;
+	CamPos.z = 0;
+
+	LookAt.x = 0;
+	LookAt.y = 0;
+	LookAt.z = 0;
+
 }
--- Anim19_OSDN/Base_Camera.h
+++ Anim19_OSDN/Base_Camera.h
@@ -4,5 +4,11 @@ class Base_Camera
 public:
 	Base_Camera();
 	~Base_Camera();
+
+	void Init_Camera(void);
+
+	Ogre::Vector3 CamPos;
+	Ogre::Vector3 LookAt;
+
 };
 
