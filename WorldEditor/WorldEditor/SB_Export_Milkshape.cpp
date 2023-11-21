db18318b3a3ef4d94ccdd62ec51ed6a9dd9dc8ed
Terry Flanigan <119083506+hgtterry@users.noreply.github.com>
2023-11-21 17:09:26 +0000

    World Editor V1.27 21-11-23 Export Milkshape

--- WorldEditor/WorldEditor/SB_Export_Milkshape.cpp
+++ WorldEditor/WorldEditor/SB_Export_Milkshape.cpp
@@ -27,8 +27,412 @@ distribution.
 
 SB_Export_Milkshape::SB_Export_Milkshape()
 {
+	OutputFolder[0] = 0;
+	Text_FileName[0] = 0;
+	MaterialName[0] = 0;
+
+	WriteMILK = nullptr;
 }
 
 SB_Export_Milkshape::~SB_Export_Milkshape()
 {
+}
+
+// *************************************************************************
+// *			Export_To_Milk:- Terry and Hazel Flanigan 2023             *
+// *************************************************************************
+bool SB_Export_Milkshape::Export_To_Milk(bool DoMotions)
+{
+	strcpy(App->CLSB_FileIO->BrowserMessage, "Select Folder To Place Milk Files a sub folder will be created");
+	int Test = App->CLSB_FileIO->StartBrowser("");
+
+	if (Test == 0)
+	{
+		return 0;
+	}
+
+	OutputFolder[0] = 0;
+
+	char buff[1024];
+	strcpy(buff, App->CLSB_FileIO->szSelectedDir);
+	strcat(buff, "\\");
+	strcat(buff, App->CLSB_Model->JustName);
+	strcat(buff, "_Milkshape");
+	strcat(buff, "\\");
+
+	strcpy(OutputFolder, buff);
+
+	CreateDirectory(OutputFolder, NULL);
+
+
+	if (App->CLSB_Model->Model_Type == Enums::LoadedFile_Assimp)
+	{
+		//App->CL_Textures->DecompileTextures(OutputFolder);
+
+		Write_MILK_File();
+	}
+
+	//CleanUp();
+
+	return 1;
+}
+
+// *************************************************************************
+// *			Write_MILKFile:- Terry and Hazel Flanigan 2023	 		   *
+// *************************************************************************
+bool SB_Export_Milkshape::Write_MILK_File(void)
+{
+	char buf[244];
+	strcpy(buf, OutputFolder);
+	strcat(buf, "\\");
+	strcat(buf, App->CLSB_Model->JustName);
+	strcat(buf, ".ms3d");
+
+	WriteMILK = fopen(buf, "wb");
+	if (!WriteMILK)
+	{
+		return 0;
+	}
+
+	word Zero = 0;
+
+	Write_MILK_Header();
+
+	float mFrames = 0;
+	float CurrentTime = 0;
+
+	int TotalFrames = 0;
+
+	Convert_To_GlobalMesh();
+
+	Write_MILK_Mesh();
+	Write_MILK_Groups();
+	Write_MILK_Texures();
+
+	fwrite(&mFrames, sizeof(float), 1, WriteMILK);
+	fwrite(&CurrentTime, sizeof(float), 1, WriteMILK);
+	fwrite(&TotalFrames, sizeof(int), 1, WriteMILK);
+
+	fwrite(&Zero, sizeof(word), 1, WriteMILK);
+
+	fclose(WriteMILK);
+
+	return 1;
+}
+
+// *************************************************************************
+// *		Write_MILKHeader:- Terry and Hazel Flanigan 2023		 	   *
+// *************************************************************************
+void SB_Export_Milkshape::Write_MILK_Header(void)
+{
+	Cms3d_header_t header;
+
+	strcpy(header.id, "MS3D000000");
+	header.version = 4;
+	fwrite(&header, sizeof(Cms3d_header_t), 1, WriteMILK);
+}
+
+// *************************************************************************
+// *		Convert_To_GlobalMesh:- Terry and Hazel Flanigan 2023  	 	   *
+// *************************************************************************
+void SB_Export_Milkshape::Convert_To_GlobalMesh(void)
+{
+	int Count = 0;
+	int GroupVertCount = 0;
+	int GroupFaceCount = 0;
+	int VC = 0;
+
+	int Offset = 0;
+
+	App->CLSB_Model->vertex_Data.resize(App->CLSB_Model->VerticeCount);
+	App->CLSB_Model->Face_Data.resize(App->CLSB_Model->FaceCount);
+
+	App->CLSB_Model->Normal_Data.resize(App->CLSB_Model->VerticeCount);
+	App->CLSB_Model->MapCord_Data.resize(App->CLSB_Model->VerticeCount);
+	App->CLSB_Model->MatIndex_Data.resize(App->CLSB_Model->VerticeCount);
+
+	while (Count < App->CLSB_Model->GroupCount)
+	{
+		GroupVertCount = 0;
+
+		while (GroupVertCount < App->CLSB_Model->Group[Count]->GroupVertCount)
+		{
+			App->CLSB_Model->vertex_Data[VC].x = App->CLSB_Model->Group[Count]->vertex_Data[GroupVertCount].x;
+			App->CLSB_Model->vertex_Data[VC].y = App->CLSB_Model->Group[Count]->vertex_Data[GroupVertCount].y;
+			App->CLSB_Model->vertex_Data[VC].z = App->CLSB_Model->Group[Count]->vertex_Data[GroupVertCount].z;
+
+			App->CLSB_Model->MapCord_Data[VC].u = App->CLSB_Model->Group[Count]->MapCord_Data[GroupVertCount].u;
+			App->CLSB_Model->MapCord_Data[VC].v = App->CLSB_Model->Group[Count]->MapCord_Data[GroupVertCount].v;
+
+			App->CLSB_Model->Normal_Data[VC].x = App->CLSB_Model->Group[Count]->Normal_Data[GroupVertCount].x;
+			App->CLSB_Model->Normal_Data[VC].y = App->CLSB_Model->Group[Count]->Normal_Data[GroupVertCount].y;
+			App->CLSB_Model->Normal_Data[VC].z = App->CLSB_Model->Group[Count]->Normal_Data[GroupVertCount].z;
+
+			VC++;
+
+			GroupVertCount++;
+		}
+
+		Count++;
+	}
+
+	VC = 0;
+	Count = 0;
+	int mFaceCount = 0;
+	while (Count < App->CLSB_Model->GroupCount)
+	{
+		mFaceCount = 0;
+		while (mFaceCount < App->CLSB_Model->Group[Count]->GroupFaceCount)
+		{
+
+			App->CLSB_Model->Face_Data[VC].Group = Count;
+
+			App->CLSB_Model->Face_Data[VC].a = App->CLSB_Model->Group[Count]->Face_Data[mFaceCount].a + Offset;
+			App->CLSB_Model->Face_Data[VC].b = App->CLSB_Model->Group[Count]->Face_Data[mFaceCount].b + Offset;
+			App->CLSB_Model->Face_Data[VC].c = App->CLSB_Model->Group[Count]->Face_Data[mFaceCount].c + Offset;
+
+
+			VC++;
+			mFaceCount++;
+		}
+
+		Offset = Offset + App->CLSB_Model->Group[Count]->GroupVertCount;
+		Count++;
+
+	}
+
+	VC = 0;
+	Count = 0;
+	while (Count < App->CLSB_Model->GroupCount)
+	{
+		GroupFaceCount = 0;
+
+		while (GroupFaceCount < App->CLSB_Model->Group[Count]->GroupFaceCount)
+		{
+			App->CLSB_Model->MatIndex_Data[VC] = App->CLSB_Model->Group[Count]->MaterialIndex;
+
+			VC++;
+			GroupFaceCount++;
+		}
+
+		Count++;
+	}
+}
+
+// *************************************************************************
+// *			WriteMILKMesh:- Terry and Hazel Flanigan 2023		 	   *
+// *************************************************************************
+bool SB_Export_Milkshape::Write_MILK_Mesh(void)
+{
+
+	word numMesh = App->CLSB_Model->VerticeCount;
+
+	fwrite(&numMesh, 2, 1, WriteMILK);
+
+	//-------------------------------------------- Vertices
+	Cms3d_Vector3_t Mesh;
+
+	int Count = 0;
+	while (Count < App->CLSB_Model->VerticeCount)
+	{
+		Mesh.Vector3[0] = App->CLSB_Model->vertex_Data[Count].x;
+		Mesh.Vector3[1] = App->CLSB_Model->vertex_Data[Count].y;
+		Mesh.Vector3[2] = App->CLSB_Model->vertex_Data[Count].z;;
+
+		Mesh.flags = 0;
+
+		Mesh.jointId = -1;
+
+		Mesh.referenceCount = 0;
+
+		fwrite(&Mesh, sizeof(Cms3d_Vector3_t), 1, WriteMILK);
+		Count++;
+	}
+
+	//-------------------------------------------- Faces
+	word numFaces = App->CLSB_Model->FaceCount;
+
+	fwrite(&numFaces, 2, 1, WriteMILK);
+
+	Cms3d_triangle_t Face;
+	Count = 0;
+
+	//App->Say_Int(App->CL_Model_Data->FaceCount);
+
+	while (Count < App->CLSB_Model->FaceCount)
+	{
+		int A = App->CLSB_Model->Face_Data[Count].a;
+		int B = App->CLSB_Model->Face_Data[Count].b;
+		int C = App->CLSB_Model->Face_Data[Count].c;
+
+		Face.Vector3Indices[0] = App->CLSB_Model->Face_Data[Count].a;
+		Face.Vector3Indices[1] = App->CLSB_Model->Face_Data[Count].b;
+		Face.Vector3Indices[2] = App->CLSB_Model->Face_Data[Count].c;
+
+		Face.flags = 0;
+
+		// 3 Face UVs for Face
+		Face.s[0] = App->CLSB_Model->MapCord_Data[A].u;
+		Face.t[0] = 1 - App->CLSB_Model->MapCord_Data[A].v;
+
+		Face.s[1] = App->CLSB_Model->MapCord_Data[B].u;
+		Face.t[1] = 1 - App->CLSB_Model->MapCord_Data[B].v;
+
+		Face.s[2] = App->CLSB_Model->MapCord_Data[C].u;
+		Face.t[2] = 1 - App->CLSB_Model->MapCord_Data[C].v;
+
+
+		// 3 Face Normals for Face
+		Face.Vector3Normals[0][0] = App->CLSB_Model->Normal_Data[A].x;
+		Face.Vector3Normals[0][1] = App->CLSB_Model->Normal_Data[A].y;
+		Face.Vector3Normals[0][2] = App->CLSB_Model->Normal_Data[A].z;
+
+		Face.Vector3Normals[1][0] = App->CLSB_Model->Normal_Data[B].x;
+		Face.Vector3Normals[1][1] = App->CLSB_Model->Normal_Data[B].y;
+		Face.Vector3Normals[1][2] = App->CLSB_Model->Normal_Data[B].z;
+
+		Face.Vector3Normals[2][0] = App->CLSB_Model->Normal_Data[C].x;
+		Face.Vector3Normals[2][1] = App->CLSB_Model->Normal_Data[C].y;
+		Face.Vector3Normals[2][2] = App->CLSB_Model->Normal_Data[C].z;
+
+		Face.groupIndex = App->CLSB_Model->Face_Data[Count].Group;
+
+		fwrite(&Face, sizeof(Cms3d_triangle_t), 1, WriteMILK);
+
+		Count++;
+	}
+
+	return 1;
+}
+
+// *************************************************************************
+// *		WriteMILKGroups_Assimp:- Terry and Hazel Flanigan 2023		   *
+// *************************************************************************
+bool SB_Export_Milkshape::Write_MILK_Groups(void)
+{
+
+	Sort_Groups();
+
+	word numGroups = App->CLSB_Model->Get_Groupt_Count();
+
+	fwrite(&numGroups, 2, 1, WriteMILK);
+
+	int Count = 0;
+	int GroupCount = App->CLSB_Model->Get_Groupt_Count();
+	while (Count < GroupCount)
+	{
+		fwrite(&TGroup[Count]->flags, sizeof(byte), 1, WriteMILK);
+		fwrite(&TGroup[Count]->name, sizeof(char), 32, WriteMILK);
+		fwrite(&TGroup[Count]->numtriangles, sizeof(word), 1, WriteMILK);
+
+		for (int j = 0; j < TGroup[Count]->numtriangles; j++)
+		{
+			fwrite(&TGroup[Count]->triangleIndices[j], 2, 1, WriteMILK);
+		}
+
+		fwrite(&TGroup[Count]->materialIndex, 1, 1, WriteMILK);
+
+		Count++;
+	}
+
+	return 1;
+}
+
+// *************************************************************************
+// *				SortGroups:- Terry and Hazel Flanigan 2023   		   *
+// *************************************************************************
+bool SB_Export_Milkshape::Sort_Groups(void)
+{
+	int IndiceCount = 0;
+	int VertCount = 0;
+	int Count = 0;
+	int GroupCount = App->CLSB_Model->Get_Groupt_Count();
+
+	while (Count < GroupCount)
+	{
+		TGroup[Count] = new Cms3d_group_t;
+		TGroup[Count]->flags = 0;
+		TGroup[Count]->materialIndex = App->CLSB_Model->Group[Count]->MaterialIndex;// -1;Count;//-1;
+		strcpy(TGroup[Count]->name, App->CLSB_Model->Group[Count]->GroupName);
+
+		VertCount = 0;
+		IndiceCount = 0;
+
+		while (VertCount < App->CLSB_Model->Group[Count]->GroupFaceCount)
+		{
+
+			TGroup[Count]->triangleIndices[IndiceCount] = App->CLSB_Model->Group[Count]->FaceIndex_Data[VertCount].Index;
+
+			IndiceCount++;
+			VertCount++;
+		}
+
+		TGroup[Count]->numtriangles = IndiceCount;
+
+		Count++;
+	}
+
+	return 1;
+}
+
+// *************************************************************************
+// *		WriteMILKTexures:- Terry and Hazel Flanigan 2023		 	   *
+// *************************************************************************
+bool SB_Export_Milkshape::Write_MILK_Texures(void)
+{
+	word numMaterials = App->CLSB_Model->Get_Groupt_Count();
+
+	fwrite(&numMaterials, 2, 1, WriteMILK);
+
+	Cms3d_material_t mat;
+
+	int Count = 0;
+	int MatCount = App->CLSB_Model->Get_Groupt_Count();
+
+	while (Count < MatCount)
+	{
+		Get_Material_Name(Count);
+		strcpy(mat.name, MaterialName);
+		SetVec4(mat.diffuse, 1, 1, 1, 1);
+		SetVec4(mat.ambient, 1, 1, 1, 1);
+		SetVec4(mat.specular, 0, 0, 0, 1);
+		SetVec4(mat.emissive, 0, 0, 0, 1);
+		mat.shininess = 0;
+		mat.transparency = 1;
+		mat.mode = 0;
+
+
+		int Len = strlen(Text_FileName);
+		Text_FileName[Len - 4] = 0;
+		strcat(Text_FileName, ".jpg");
+
+		strcpy(mat.texture, Text_FileName);
+		strcpy(mat.alphamap, "");
+
+		fwrite(&mat, sizeof(Cms3d_material_t), 1, WriteMILK);
+		Count++;
+	}
+	return 1;
+}
+
+// *************************************************************************
+// *			GetMaterialName:- Terry and Hazel Flanigan 2023		 	   *
+// *************************************************************************
+bool SB_Export_Milkshape::Get_Material_Name(int Loop)
+{
+
+	strcpy(MaterialName, App->CLSB_Model->Group[Loop]->MaterialName);
+	strcpy(Text_FileName, App->CLSB_Model->Group[Loop]->Text_FileName);
+	return 1;
+}
+
+// *************************************************************************
+// *			SetVec4:- Terry and Hazel Flanigan 2023  				   *
+// *************************************************************************
+inline void SB_Export_Milkshape::SetVec4(float* target, float x, float y, float z, float w)
+{
+	target[0] = x;
+	target[1] = y;
+	target[2] = z;
+	target[3] = w;
 }
--- WorldEditor/WorldEditor/SB_Export_Milkshape.h
+++ WorldEditor/WorldEditor/SB_Export_Milkshape.h
@@ -22,10 +22,88 @@ misrepresented as being the original software.
 distribution.
 */
 
+#include <pshpack1.h>
+
+#ifndef word
+typedef unsigned short word;
+#endif // word
+
+typedef struct
+{
+	char    id[10];                                     // always "MS3D000000"
+	int     version;                                    // 4
+} Cms3d_header_t;
+
+struct Cms3d_Vector3_t
+{
+	byte    flags;                                      // SELECTED | SELECTED2 | HIDDEN
+	float   Vector3[3];                                  // x, y, z
+	char    jointId;                                     // -1 = no joint
+	byte    referenceCount;
+};
+
+struct Cms3d_triangle_t
+{
+	word    flags;                                      // SELECTED | SELECTED2 | HIDDEN
+	word    Vector3Indices[3];                           //
+	float   Vector3Normals[3][3];                        //
+	float   s[3];                                       // texture coord s 
+	float   t[3];                                       // texture coord t
+	byte    smoothingGroup;                             // 1 - 32
+	byte    groupIndex;                                 //
+};
+
+typedef struct
+{
+	byte            flags;                              // SELECTED | HIDDEN
+	char            name[32];                           //
+	word            numtriangles;                       //
+	word			triangleIndices[65535];					// the groups group the triangles
+	char            materialIndex;                      // -1 = no material
+} Cms3d_group_t;
+
+typedef struct
+{
+	char            name[32];                           //
+	float           ambient[4];                         //
+	float           diffuse[4];                         //
+	float           specular[4];                        //
+	float           emissive[4];                        //
+	float           shininess;                          // 0.0f - 128.0f
+	float           transparency;                       // 0.0f - 1.0f
+	char            mode;                               // 0, 1, 2 is unused now
+	char            texture[128];                        // texture.bmp
+	char            alphamap[128];                       // alpha.bmp
+} Cms3d_material_t;
+
+#include <poppack.h>
+
 class SB_Export_Milkshape
 {
 public:
 	SB_Export_Milkshape();
 	~SB_Export_Milkshape();
+
+	bool Export_To_Milk(bool DoMotions);
+
+private:
+
+	bool Write_MILK_File(void);
+	void Write_MILK_Header(void);
+	void Convert_To_GlobalMesh(void);
+	bool Write_MILK_Mesh(void);
+	bool Write_MILK_Groups(void);
+	bool Sort_Groups(void);
+	bool Write_MILK_Texures(void);
+	bool Get_Material_Name(int Loop);
+	inline void SetVec4(float* target, float x, float y, float z, float w);
+
+	char OutputFolder[MAX_PATH];
+	char Text_FileName[MAX_PATH];
+	char MaterialName[MAX_PATH];
+
+	Cms3d_group_t* TGroup[200];
+
+	FILE* WriteMILK;
 };
 
